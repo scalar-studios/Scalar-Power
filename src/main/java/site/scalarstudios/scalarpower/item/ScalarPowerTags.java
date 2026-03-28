20633ed89b5520e4dc49f8cfa4138e61c41ec4a2
Lemon-Juiced <47193272+Lemon-Juiced@users.noreply.github.com>
2026-03-27 23:44:45 -0400

    Tags and Loot Tables

--- src/main/java/site/scalarstudios/scalarpower/item/ScalarPowerTags.java
+++ src/main/java/site/scalarstudios/scalarpower/item/ScalarPowerTags.java
@@ -4,12 +4,8 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
-import site.scalarstudios.scalarpower.ScalarPower;
 
 public class ScalarPowerTags {
-    public static final TagKey<Item> RAW_IRON_GRINDABLE = TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(ScalarPower.MODID, "raw_iron_grindable"));
-    public static final TagKey<Item> RAW_GOLD_GRINDABLE = TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(ScalarPower.MODID, "raw_gold_grindable"));
-    public static final TagKey<Item> RAW_COPPER_GRINDABLE = TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(ScalarPower.MODID, "raw_copper_grindable"));
     public static final TagKey<Item> C_RAW_MATERIALS = TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("c", "raw_materials"));
     public static final TagKey<Item> C_INGOTS = TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("c", "ingots"));
 }
