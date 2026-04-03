f8e2191a51a964c7d70508760ffe44467cf55af8
Lemon-Juiced <47193272+Lemon-Juiced@users.noreply.github.com>
2026-04-02 21:36:33 -0400

    JEI bonus outputs noted for Grinder

--- src/main/java/site/scalarstudios/scalarpower/integration/jei/category/GrindingRecipeCategory.java
+++ src/main/java/site/scalarstudios/scalarpower/integration/jei/category/GrindingRecipeCategory.java
@@ -10,6 +10,7 @@ import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.category.IRecipeCategory;
 import mezz.jei.api.recipe.types.IRecipeHolderType;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
@@ -20,6 +21,8 @@ import site.scalarstudios.scalarpower.ScalarPower;
 import site.scalarstudios.scalarpower.block.ScalarPowerBlocks;
 import site.scalarstudios.scalarpower.recipe.GrindingRecipe;
 import site.scalarstudios.scalarpower.recipe.ScalarPowerRecipes;
+
+import java.util.Locale;
 
 public class GrindingRecipeCategory implements IRecipeCategory<RecipeHolder<GrindingRecipe>> {
     private static final Identifier TEXTURE = Identifier.fromNamespaceAndPath(ScalarPower.MODID, "textures/gui/jei_generic_1to1.png");
@@ -64,15 +67,38 @@ public class GrindingRecipeCategory implements IRecipeCategory<RecipeHolder<Grin
     @SuppressWarnings("removal") // This will need to be addressed at some point
     @Override
     public void setRecipe(IRecipeLayoutBuilder builder, RecipeHolder<GrindingRecipe> recipe, IFocusGroup focuses) {
+        GrindingRecipe grindingRecipe = recipe.value();
+
         builder.addSlot(RecipeIngredientRole.INPUT, 56, 35)
-                .addIngredients(recipe.value().input());
+                .addIngredients(grindingRecipe.input());
 
         builder.addSlot(RecipeIngredientRole.OUTPUT, 116, 35)
-                .addItemStack(recipe.value().assemble(new SingleRecipeInput(ItemStack.EMPTY)));
+                .addItemStack(grindingRecipe.assemble(new SingleRecipeInput(ItemStack.EMPTY)));
     }
 
     @Override
     public void draw(RecipeHolder<GrindingRecipe> recipe, IRecipeSlotsView recipeSlotsView, GuiGraphicsExtractor guiGraphics, double mouseX, double mouseY) {
         background.draw(guiGraphics);
+
+        GrindingRecipe grindingRecipe = recipe.value();
+        if (grindingRecipe.bonusChance() <= 0.0F) {
+            return;
+        }
+
+        String chanceText = formatChance(grindingRecipe.bonusChance());
+        Component bonusText = Component.translatable(
+                "jei.scalarpower.grinding.bonus_output",
+                chanceText,
+                grindingRecipe.bonusCount());
+        guiGraphics.text(Minecraft.getInstance().font, bonusText, 8, 64, 0xFF4A4A4A, false);
+    }
+
+    private static String formatChance(float bonusChance) {
+        float percent = bonusChance * 100.0F;
+        if (Math.abs(percent - Math.round(percent)) < 0.0001F) {
+            return Integer.toString(Math.round(percent));
+        }
+
+        return String.format(Locale.ROOT, "%.1f", percent);
     }
 }
